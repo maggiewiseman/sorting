ff6424ce673f17333a03c6952fe651f18325e82a
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-11-26 16:01:02 -0500

    wrote a test for sorter and it passed

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  sort: function(unsorted) {
+    console.log("in sort funtion of sorter object");
+    return [1,2,3];
+  }
+}
+
--- test/test.js
+++ test/test.js
@@ -1,18 +1,10 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-
+var sorter = require('../index');
 describe('sorter', function() {
   describe('#selectionSort', function() {
-    it('should return -1 when the value is not present', function() {
-      var sorter = new Sorter();
+    it('should output a sorted array', function() {
       var sorted = sorter.sort([3,2,1]);
-      assert.equal([1,2,3], sorted);
+      assert.deepEqual([1,2,3], sorted);
     });
   });
 });

d32cd427de43110dbb7b0a851428528a5b64847d
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-11-26 16:20:54 -0500

    write a minIndex function

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 module.exports = {
-  sort: function(unsorted) {
+  selectionSort: function(unsorted) {
     console.log("in sort funtion of sorter object");
+    //write a min function for an array
+    // 3 2 1 4 2 
+    // remove the min and put it in a new array
+    
     return [1,2,3];
+  },
+  
+  minOfArrayIndex: function(myArray) {
+    var smallest = myArray[0];
+    var index = 0;
+    //compare start value to each element in array
+    //if smallest is larger than element, smallest = current element.  Index = indexOf current element
+    //return index
+    for(var i=1; i < myArray.length; i++) {
+      if (smallest > myArray[i] ) {
+        smallest = myArray[i];
+        index = i;
+      }
+    }
+    
+    return index;
   }
-}
+};
 
--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
 var assert = require('assert');
 var sorter = require('../index');
 describe('sorter', function() {
+  //test for selectionSort
   describe('#selectionSort', function() {
     it('should output a sorted array', function() {
-      var sorted = sorter.sort([3,2,1]);
+      var sorted = sorter.selectionSort([3,2,1]);
       assert.deepEqual([1,2,3], sorted);
     });
   });
+  
+  //test for minOfArrayIndex
+  describe('#minOfArrayIndex', function() {
+    it('should output index of first smallest number', function() {
+      var minIndex = sorter.minOfArrayIndex([3,2,1,4,1]);
+      assert.equal(2, minIndex);
+    });
+  });
+  
 });

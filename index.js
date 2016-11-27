26853f47a788b8d5dffc4df631ec066fce40bcec
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-11-27 09:29:35 -0500

    made bubble sort an test for bubble sort

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var Sorter = function () {
   
 };
 
-Sorter.prototype.minOfArrayIndex = function(myArray) {
+Sorter.prototype.minOfArrayIndex = function (myArray) {
   var smallest = myArray[0];
-    var index = 0;
-    //compare start value to each element in array
-    //if smallest is larger than element, smallest = current element.  Index = indexOf current element
-    //return index
-    for(var i=1; i < myArray.length; i++) {
-      if (smallest > myArray[i] ) {
-        smallest = myArray[i];
-        index = i;
-      }
+  var index = 0;
+  //compare start value to each element in array
+  //if smallest is larger than element, smallest = current element.  Index = indexOf current element
+  //return index
+  for (var i=1; i < myArray.length; i++) {
+    if (smallest > myArray[i] ) {
+      smallest = myArray[i];
+      index = i;
     }
-    
-    return index;
+  }
+
+  return index;
 };
 
 Sorter.prototype.selectionSort = function (unsorted){
@@ -27,13 +27,41 @@ Sorter.prototype.selectionSort = function (unsorted){
     // 3 2 1 4 2 
     var indexOfSmallest = 0;
     var sorted = [];
-    while(unsorted.length > 0) {
+    while (unsorted.length > 0) {
       indexOfSmallest = this.minOfArrayIndex(unsorted);
-      sorted.push(unsorted.splice(indexOfSmallest,1)); 
+      sorted.push(unsorted.splice(indexOfSmallest,1)[0]); 
     }
-    
-    
+        
     return sorted;
 };
+
+Sorter.prototype.bubbleSort = function (unsorted) {
+  console.log("in bubble sort function");
+  
+  //for length of array
+  //compare startIndex to each element
+  //if startIndex > element[i], set element [i] = to temp, then set element[i] = element[startIndex] and element[startIndex] = temp.
+  for (var j = 0; j < unsorted.length; j++){
+    var startIndex = j;
+    for (var i = startIndex + 1; i < unsorted.length; i++) {
+      if(unsorted[startIndex] > unsorted[i]) {
+        var tmp = unsorted[i];
+        unsorted[i] = unsorted[startIndex];
+        unsorted[startIndex] = tmp;
+      }
+    }
+  }
+  
+  return unsorted;
+}; 
+
+Sorter.prototype.makeRandomArray = function (size) {
+  var arr = [];
+  for (var i = 0; i < size; i++) {
+    arr.push(Math.round(Math.random()*100));
+  }
+  return arr;
+};
+
 
 module.exports = new Sorter();
--- test/test.js
+++ test/test.js
@@ -1,20 +1,38 @@
+/*jslint devel: true, sloppy: true*/
 var assert = require('assert');
 var sorter = require('../index');
-describe('sorter', function() {
+describe('sorter', function () {
   //test for selectionSort
-  describe('#selectionSort', function() {
-    it('should output a sorted array', function() {
-      var sorted = sorter.selectionSort([3,2,1]);
-      assert.deepEqual([1,2,3], sorted);
+  describe('#selectionSort', function () {
+    it('should output a sorted array', function () {
+      var sorted = sorter.selectionSort([3, 2, 1]);
+      assert.deepEqual([1, 2, 3], sorted);
     });
   });
   
   //test for minOfArrayIndex
-  describe('#minOfArrayIndex', function() {
-    it('should output index of first smallest number', function() {
-      var minIndex = sorter.minOfArrayIndex([3,2,1,4,1]);
+  describe('#minOfArrayIndex', function () {
+    it('should output index of first smallest number', function () {
+      var minIndex = sorter.minOfArrayIndex([3, 2, 1, 4, 1]);
       assert.equal(2, minIndex);
     });
   });
   
+  //performance test for selection sort
+//  describe('#selection sort performance', function () {
+//    this.timeout(120000);
+//    it('should suck pretty bad', function () {
+//      var start = new Date().getTime();   sorter.selectionSort(sorter.makeRandomArray(100000));
+//      var end = new Date().getTime();
+//      console.log("seconds: " + (end - start) / 1000);
+//    });
+//  });
+  
+  //performance test for bubble sort
+  describe('#bubble sort performance', function () {
+      it('should return a sorted array using bubble sort', function (){ 
+      var sorted = sorter.bubbleSort([3, 2, 1]);
+      assert.deepEqual([1, 2, 3], sorted);         
+    });
+  });
 });
